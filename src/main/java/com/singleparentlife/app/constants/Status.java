4ca85285954da101e3db5b49fd76696ca10c66e9
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-26 00:42:32 -0700

    Add some status codes to Status

--- src/main/java/com/singleparentlife/app/constants/Status.java
+++ src/main/java/com/singleparentlife/app/constants/Status.java
@@ -1,4 +1,26 @@
 package com.singleparentlife.app.constants;
 
 public enum Status {
+
+    SUCCESS (0, "Success"),
+
+    INVALID_INPUT (400, "Invalid input"),
+    PERMISSION_DENIED (403, "Permission is denied"),
+    ;
+
+    private final int code;
+    private final String message;
+
+    Status (int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
