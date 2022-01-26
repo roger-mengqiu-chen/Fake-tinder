fd9cf34a8ea357b77eb654516fcad3e863deb7a6
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-25 22:46:39 -0700

    Create RoleString for admin, free user, premium user

---
+++ src/main/java/com/singleparentlife/app/constants/RoleString.java
@@ -0,0 +1,7 @@
+package com.singleparentlife.app.constants;
+
+public class RoleString {
+    public final static String ADMIN = "ADMIN";
+    public final static String FREE_USER = "FREE_USER";
+    public final static String PREMIUM_USER = "PREMIUM_USER";
+}
