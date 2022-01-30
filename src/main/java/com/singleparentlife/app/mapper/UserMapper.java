c7ee357bdcf6ae592e2637c1c4d1837fbc6d9c5b
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-30 13:19:36 -0700

    Add findByEmail

--- src/main/java/com/singleparentlife/app/mapper/UserMapper.java
+++ src/main/java/com/singleparentlife/app/mapper/UserMapper.java
@@ -1,9 +1,29 @@
 package com.singleparentlife.app.mapper;
 
+import com.singleparentlife.app.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
 
+    @Select("SELECT * FROM user WHERE email = #{email}")
+    @Results ({
+            @Result(id = true, property = "userId", column = "userId"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "phone", column = "phone"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "startDate", column = "startDate"),
+            @Result(property = "loginTime", column = "loginTime"),
+            @Result(property = "roleId", column = "roleId"),
+            @Result(property = "isActive", column = "isActive"),
+            @Result(property = "isSuspended", column = "isSuspended")
+
+    })
+    Optional<User> findByEmail(String email);
 
 }
