4b57e141cd9ec6184983c077e14e42856a641fb4
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-30 13:18:22 -0700

    Create User

--- src/main/java/com/singleparentlife/app/model/User.java
+++ src/main/java/com/singleparentlife/app/model/User.java
@@ -2,6 +2,27 @@ package com.singleparentlife.app.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 public class User {
+
+    private long userId;
+
+    private String email;
+
+    private String phone;
+
+    private String password;
+
+    private LocalDate startDate;
+
+    private LocalDateTime loginTime;
+
+    private int roleId;
+
+    private boolean isActive;
+
+    private boolean isSuspended;
 }
