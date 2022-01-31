004f99597c8c5cff15b8f2369ca9e4eb57f08af8
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-30 22:26:25 -0700

    Create security configurations

---
+++ src/main/java/com/singleparentlife/app/config/security/MyUserDetails.java
@@ -0,0 +1,58 @@
+package com.singleparentlife.app.config.security;
+
+import com.singleparentlife.app.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+
+    private final User user;
+
+    private Collection<GrantedAuthority> authorities;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    public void setAuthorities (List<? extends GrantedAuthority> authorities) {
+        this.authorities = (Collection<GrantedAuthority>) authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !user.isSuspended();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
---
+++ src/main/java/com/singleparentlife/app/config/security/UsrDetailsServiceImp.java
@@ -0,0 +1,38 @@
+package com.singleparentlife.app.config.security;
+
+
+import com.singleparentlife.app.mapper.UserMapper;
+import com.singleparentlife.app.model.Role;
+import com.singleparentlife.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsrDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userMapper.findByEmail(email);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("Couldn't find user");
+        }
+
+        Role role = userMapper.getRoleOfUser(user);
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        MyUserDetails userDetails = new MyUserDetails(user);
+        userDetails.setAuthorities(authorities);
+
+        return userDetails;
+    }
+}
