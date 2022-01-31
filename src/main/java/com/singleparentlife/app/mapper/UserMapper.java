330a08f265927fa7b2c5d6d73f6fc5dc59248a44
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-30 22:29:15 -0700

    Update notations of save Add findById Add getRoleOfUser

--- src/main/java/com/singleparentlife/app/mapper/UserMapper.java
+++ src/main/java/com/singleparentlife/app/mapper/UserMapper.java
@@ -1,15 +1,19 @@
 package com.singleparentlife.app.mapper;
 
+import com.singleparentlife.app.model.Role;
 import com.singleparentlife.app.model.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.Optional;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
+
+    /* Create */
+    @Insert("INSERT INTO user (email, password, startDate, loginTime, roleId, isActive, isSuspended) VALUES " +
+            "(#{email}, #{password}, #{startDate}, #{loginTime}, #{roleId}, #{isActive}, #{isSuspended})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int save(User user);
+
+    /* Read */
 
     @Select("SELECT * FROM user WHERE email = #{email}")
     @Results ({
@@ -24,6 +28,23 @@ public interface UserMapper {
             @Result(property = "isSuspended", column = "isSuspended")
 
     })
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 
+    @Select("SELECT * FROM user WHERE userId = #{userId}")
+    @Results ({
+            @Result(id = true, property = "userId", column = "userId"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "phone", column = "phone"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "startDate", column = "startDate"),
+            @Result(property = "loginTime", column = "loginTime"),
+            @Result(property = "roleId", column = "roleId"),
+            @Result(property = "isActive", column = "isActive"),
+            @Result(property = "isSuspended", column = "isSuspended")
+
+    })
+    User findById(long userId);
+
+    @Select("SELECT * FROM role WHERE roleId = #{roleId}")
+    Role getRoleOfUser(User user);
 }
