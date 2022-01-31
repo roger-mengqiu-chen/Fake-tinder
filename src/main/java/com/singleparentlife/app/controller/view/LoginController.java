e0fdf42c51a328482f181bc5a6174e87a818daff
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-30 22:16:55 -0700

    Add view controllers

---
+++ src/main/java/com/singleparentlife/app/controller/view/LoginController.java
@@ -0,0 +1,16 @@
+package com.singleparentlife.app.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping()
+    public String getLogin(){
+        return "login";
+    }
+
+}
