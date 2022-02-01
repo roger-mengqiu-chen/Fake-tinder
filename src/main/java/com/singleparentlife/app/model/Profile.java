930164b92cd8b6294582c07eaf26415456a24b46
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-01 01:06:09 -0700

    Create Profile

--- src/main/java/com/singleparentlife/app/model/Profile.java
+++ src/main/java/com/singleparentlife/app/model/Profile.java
@@ -4,4 +4,15 @@ import lombok.Data;
 
 @Data
 public class Profile {
+
+    private long userId;
+
+    private String firstname;
+
+    private String lastname;
+
+    private short age;
+
+    private char gender;
+
 }
