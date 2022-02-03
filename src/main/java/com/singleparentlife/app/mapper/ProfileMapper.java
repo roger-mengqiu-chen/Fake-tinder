c397a966c94f57f37e1ac20d4e8122b7262a13a5
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-03 01:22:46 -0700

    Create ProfileMapper SignupRequest is no need

---
+++ src/main/java/com/singleparentlife/app/mapper/ProfileMapper.java
@@ -0,0 +1,20 @@
+package com.singleparentlife.app.mapper;
+
+import com.singleparentlife.app.model.Profile;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProfileMapper {
+
+    /* Create */
+    int save(Profile profile);
+
+    /* Read */
+
+
+    /* Update */
+
+
+    /* Delete */
+
+}
