26a2e783a15ef2b74dd802f66b1fbb8b00cdd5c8
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-03 01:11:08 -0700

    Update field

--- src/main/java/com/singleparentlife/app/mapper/UserMapper.java
+++ src/main/java/com/singleparentlife/app/mapper/UserMapper.java
@@ -8,8 +8,8 @@ import org.apache.ibatis.annotations.*;
 public interface UserMapper {
 
     /* Create */
-    @Insert("INSERT INTO user (email, password, startDate, loginTime, roleId, isActive, isSuspended) VALUES " +
-            "(#{email}, #{password}, #{startDate}, #{loginTime}, #{roleId}, #{isActive}, #{isSuspended})")
+    @Insert("INSERT INTO user (fireId, email, password, startDate, loginTime, roleId, isActive, isSuspended) VALUES " +
+            "(#{fireId}, #{email}, #{password}, #{startDate}, #{loginTime}, #{roleId}, #{isActive}, #{isSuspended})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     int save(User user);
 
@@ -18,6 +18,7 @@ public interface UserMapper {
     @Select("SELECT * FROM user WHERE email = #{email}")
     @Results ({
             @Result(id = true, property = "userId", column = "userId"),
+            @Result(property = "fireId", column = "fireId"),
             @Result(property = "email", column = "email"),
             @Result(property = "phone", column = "phone"),
             @Result(property = "password", column = "password"),
@@ -33,6 +34,7 @@ public interface UserMapper {
     @Select("SELECT * FROM user WHERE userId = #{userId}")
     @Results ({
             @Result(id = true, property = "userId", column = "userId"),
+            @Result(property = "fireId", column = "fireId"),
             @Result(property = "email", column = "email"),
             @Result(property = "phone", column = "phone"),
             @Result(property = "password", column = "password"),
@@ -44,6 +46,22 @@ public interface UserMapper {
 
     })
     User findById(long userId);
+
+    @Select("SELECT * FROM user WHERE fireId = #{fireId}")
+    @Results ({
+            @Result(id = true, property = "userId", column = "userId"),
+            @Result(property = "fireId", column = "fireId"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "phone", column = "phone"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "startDate", column = "startDate"),
+            @Result(property = "loginTime", column = "loginTime"),
+            @Result(property = "roleId", column = "roleId"),
+            @Result(property = "isActive", column = "isActive"),
+            @Result(property = "isSuspended", column = "isSuspended")
+
+    })
+    User findByFireId(String fireId);
 
     @Select("SELECT * FROM role WHERE roleId = #{roleId}")
     Role getRoleOfUser(User user);
