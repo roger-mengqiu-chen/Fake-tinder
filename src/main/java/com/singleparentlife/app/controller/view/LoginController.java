b04f2461ed2c1e9944fe54b09d675d56b4c0acc4
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-03 01:21:16 -0700

    Create LoginController and TestController

--- src/main/java/com/singleparentlife/app/controller/view/LoginController.java
+++ src/main/java/com/singleparentlife/app/controller/view/LoginController.java
@@ -1,16 +1,54 @@
 package com.singleparentlife.app.controller.view;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.singleparentlife.app.config.security.UserDetailsServiceImp;
+import com.singleparentlife.app.model.User;
+import com.singleparentlife.app.payload.request.LoginRequest;
+import com.singleparentlife.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/login")
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/enter")
 public class LoginController {
 
-    @GetMapping()
-    public String getLogin(){
-        return "login";
+    @Autowired
+    UserDetailsServiceImp userDetailsServiceImp;
+    @Autowired
+    UserService userService;
+    @PostMapping
+    public void login(@RequestBody LoginRequest request){
+        String token = request.getToken();
+
+        UserDetails user = userDetailsServiceImp.loadUserByUsername(token);
+
+        if (user == null) {
+            try {
+                FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                String uid = decodedToken.getUid();
+                User newUser = new User();
+                newUser.setFireId(uid);
+                newUser.setLoginTime(LocalDateTime.now());
+                newUser.setStartDate(LocalDate.now());
+                newUser.setRoleId(2);
+                newUser.setActive(true);
+                newUser.setSuspended(false);
+                userService.saveUser(newUser);
+            } catch (FirebaseAuthException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
