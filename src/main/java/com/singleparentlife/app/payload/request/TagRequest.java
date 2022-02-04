4f38221a073c5d29289f6daeee9ef783cffe8d72
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-04 16:51:14 -0700

    Update TagRequest

--- src/main/java/com/singleparentlife/app/payload/request/TagRequest.java
+++ src/main/java/com/singleparentlife/app/payload/request/TagRequest.java
@@ -3,10 +3,12 @@ package com.singleparentlife.app.payload.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class TagRequest {
 
     @JsonProperty
-    private String tagName;
+    private List<String> tagNames;
 
 }
