2dfad2c90ddd4bfc9c68cd21c1740d113186650b
MarcSchmidtSait <marc.schmidt@edu.sait.ca>
2022-02-04 16:11:29 -0700

    Updated profile ProfileMapper

--- src/main/java/com/singleparentlife/app/config/security/SecurityFilter.java
+++ src/main/java/com/singleparentlife/app/config/security/SecurityFilter.java
@@ -5,7 +5,6 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.singleparentlife.app.constants.HeaderName;
 import com.singleparentlife.app.model.User;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
