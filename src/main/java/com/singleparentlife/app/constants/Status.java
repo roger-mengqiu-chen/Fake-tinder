423b6b76d18e2538b137e98acac90fdcca70a4d0
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-06 16:37:59 -0700

    Simplify Status

--- src/main/java/com/singleparentlife/app/constants/Status.java
+++ src/main/java/com/singleparentlife/app/constants/Status.java
@@ -2,26 +2,6 @@ package com.singleparentlife.app.constants;
 
 public enum Status {
 
-    SUCCESS ("Success"),
-    FAIL("Fail"),
-
-    INVALID_INPUT ("Invalid input"),
-    PERMISSION_DENIED ("Permission is denied"),
-    FETCH_TOKEN_FAILED("Didn't get firebase token"),
-
-    PREFERENCES_PARTIALLY_CREATED("Preferences are not fully created"),
-    PREFERENCE_NOT_FOUND("The required preference is not found"),
-    ;
-
-
-    private final String message;
-
-    Status (String message) {
-
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    SUCCESS,
+    FAIL
 }
