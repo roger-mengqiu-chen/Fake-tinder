67298a988d555bc4abc8235f948d729943a97d0b
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-07 11:57:09 -0700

    Update preferenceSerice

--- src/main/java/com/singleparentlife/app/service/PreferenceService.java
+++ src/main/java/com/singleparentlife/app/service/PreferenceService.java
@@ -1,5 +1,6 @@
 package com.singleparentlife.app.service;
 
+import com.singleparentlife.app.Util.AuthUtil;
 import com.singleparentlife.app.constants.DataType;
 import com.singleparentlife.app.constants.Status;
 import com.singleparentlife.app.mapper.PreferenceMapper;
@@ -9,60 +10,77 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @Slf4j
 public class PreferenceService {
 
     @Autowired
     private PreferenceMapper preferenceMapper;
+    @Autowired
+    private AuthUtil authUtil;
 
-    public JsonResponse createPreferenceFromList(List<String> preferences) {
-        List<String> errors = new ArrayList<>();
-        boolean haveError = false;
-        for (String p : preferences) {
-            Preference preference = preferenceMapper.findPreferenceByContent(p);
-            if (preference == null) {
-                try {
-                    preference = new Preference();
-                    preference.setContent(p);
-                    preferenceMapper.save(preference);
-                    log.info("Preference is saved: {}", p);
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                    haveError = true;
-                    errors.add(p);
-                    log.error("ERROR with saving preference: {}", p);
-                }
+    public JsonResponse createPreference (String preferenceStr) {
+        Preference preference = preferenceMapper.findByContent(preferenceStr);
+        if (preference == null) {
+            try {
+                preference = new Preference(preferenceStr);
+                preferenceMapper.save(preference);
+                // we need the persisted preference with perferenceId
+                preference = preferenceMapper.findByContent(preferenceStr);
+                log.info("Preference is saved: {}", preferenceStr);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
             }
         }
-        // If we found errors, we need to tell controller.
-        if (haveError) {
-            return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, errors);
-        }
-
-        List<Preference> preferenceList = preferences.stream().map(
-                e -> preferenceMapper.findPreferenceByContent(e)).collect(Collectors.toList());
-
-        return new JsonResponse(Status.SUCCESS, DataType.PREFERENCES, preferenceList);
+        return new JsonResponse(Status.SUCCESS, DataType.PREFERENCE, preference);
     }
 
-    public JsonResponse getPreferenceByContent (String name) {
+    /*
+        Create preference for user
+        If the preference doesn't exist, create it.
+     */
+    public JsonResponse createPreferenceOrTagForUser (String preferenceStr, DataType dataType) {
 
-        Preference preference = preferenceMapper.findPreferenceByContent(name);
+        long userId = authUtil.getCurrentUserId();
+
+        JsonResponse response = createPreference(preferenceStr);
+        if (response.getStatus().equals(Status.SUCCESS)) {
+            Preference preference = (Preference)response.getData();
+            if (dataType.equals(DataType.PREFERENCE)) {
+                preferenceMapper.savePreferenceForUser(userId, preference.getPreferenceId());
+            }
+            else if (dataType.equals(DataType.TAG)) {
+                preferenceMapper.saveTagForUser(userId, preference.getPreferenceId());
+            }
+            else {
+                log.error("Invalid dataType, should be PREFERENCE or TAG");
+                return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, "Invalid dataType, should be PREFERENCE or TAG");
+            }
+            log.info("Preference is created: {}", preferenceStr);
+            return new JsonResponse(Status.SUCCESS, DataType.PREFERENCE, preference);
+        }
+        else {
+            return response;
+        }
+    }
+
+    public JsonResponse getPreferenceByContent (String content) {
+        Preference preference = preferenceMapper.findByContent(content);
         return new JsonResponse(Status.SUCCESS, DataType.PREFERENCE, preference);
-
     }
 
     public JsonResponse updatePreferenceByContent (String content, String updatedContent) {
         try {
-            Preference preference = preferenceMapper.findPreferenceByContent(content);
+            Preference preference = preferenceMapper.findByContent(content);
+            Preference existedPreference = preferenceMapper.findByContent(updatedContent);
             if (preference == null) {
                 log.error("Preference not found: {}", content);
                 return new JsonResponse(Status.FAIL, DataType.PREFERENCE_NOT_FOUND, null);
+            }
+            else if (existedPreference != null) {
+                log.error("Updated preference already existed: {}", updatedContent);
+                return new JsonResponse(Status.FAIL, DataType.PREFERENCE_EXISTED, existedPreference);
             }
             else {
                 preference.setContent(updatedContent);
@@ -72,19 +90,19 @@ public class PreferenceService {
             }
         } catch (Exception e) {
             log.error(e.getMessage());
-            return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR,null);
+            return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
         }
     }
 
     public JsonResponse deletePreferenceByContent (String content) {
 
-        Preference preference = preferenceMapper.findPreferenceByContent(content);
+        Preference preference = preferenceMapper.findByContent(content);
         if (preference == null) {
             return new JsonResponse(Status.FAIL, DataType.PREFERENCE_NOT_FOUND, null);
         }
 
         try {
-            preferenceMapper.deletePreferenceByContent(preference);
+            preferenceMapper.deletePreference(preference);
             log.info("Preference is deleted: {}", preference.getPreferenceId());
             return new JsonResponse(Status.SUCCESS, DataType.PREFERENCE, preference);
         } catch (Exception e) {
@@ -92,4 +110,32 @@ public class PreferenceService {
             return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
         }
     }
+
+    public JsonResponse deletePreferenceOrTagForUser(Long preferenceId, DataType dataType) {
+
+        long userId = authUtil.getCurrentUserId();
+
+        Preference preference = preferenceMapper.findById(preferenceId);
+        if (preference == null) {
+            log.error("Preference id not found: {}", preferenceId);
+            return new JsonResponse(Status.FAIL, DataType.PREFERENCE_NOT_FOUND, null);
+        }
+        try {
+            if (dataType.equals(DataType.PREFERENCE)) {
+                preferenceMapper.deleteUserPreference(userId, preferenceId);
+            }
+            else if (dataType.equals(DataType.TAG)) {
+                preferenceMapper.deleteUserTag(userId, preferenceId);
+            }
+            else {
+                log.error("Invalid dataType, should be PREFERENCE or TAG");
+                return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, "Invalid dataType, should be PREFERENCE or TAG");
+            }
+            return new JsonResponse(Status.SUCCESS, DataType.PREFERENCE, preference);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
+        }
+    }
+
 }
