36c37a96429ea35551dcf82fbc6c351a8814c9cd
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-06 17:44:40 -0700

    Change Class name

--- src/main/java/com/singleparentlife/app/config/security/FirebaseFilter.java
+++ src/main/java/com/singleparentlife/app/config/security/FirebaseFilter.java
@@ -3,15 +3,12 @@ package com.singleparentlife.app.config.security;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
-import com.singleparentlife.app.constants.HeaderName;
-import com.singleparentlife.app.model.User;
-import com.singleparentlife.app.service.UserService;
+import com.singleparentlife.app.constants.SecurityConstant;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -31,7 +28,7 @@ public class FirebaseFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String xAuth = request.getHeader(HeaderName.FIRE_AUTH);
+        String xAuth = request.getHeader(SecurityConstant.FIRE_AUTH);
 
         if (xAuth == null) {
             filterChain.doFilter(request, response);
