e2cd5ee0bf1ec91ea42930f21d075bb220c0e0d9
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-07 10:35:12 -0700

    Add component AuthUtil so fetching userId from current user can be easier

---
+++ src/main/java/com/singleparentlife/app/Util/AuthUtil.java
@@ -0,0 +1,21 @@
+package com.singleparentlife.app.Util;
+
+import com.singleparentlife.app.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String fireId = authentication.getName();
+        Long userId = userMapper.getUserIdByFireId(fireId);
+        return userId;
+    }
+}
