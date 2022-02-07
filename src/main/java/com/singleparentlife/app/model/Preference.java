8ea072a158b283d788b12f4a13ee7904d6639473
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-07 10:36:38 -0700

    Add constructor

--- src/main/java/com/singleparentlife/app/model/Preference.java
+++ src/main/java/com/singleparentlife/app/model/Preference.java
@@ -1,11 +1,17 @@
 package com.singleparentlife.app.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Preference {
 
     private long preferenceId;
 
     private String content;
+
+    public Preference(String content) {
+        this.content = content;
+    }
 }
