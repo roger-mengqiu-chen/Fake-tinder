525d09b766302ec69b99b370bd154cfd02d4d6c9
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-10 16:10:16 -0700

    Add reaction constants

---
+++ src/main/java/com/singleparentlife/app/constants/ReactionString.java
@@ -0,0 +1,7 @@
+package com.singleparentlife.app.constants;
+
+public class ReactionString {
+    public static String ACCEPT = "accept";
+    public static String DENY = "deny";
+    public static String PEND = "pend";
+}
