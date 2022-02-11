7ad6a6ee5b02e3d05e9f4a508741e0b6f46f3115
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-11 09:46:29 -0700

    Update payload

--- src/main/java/com/singleparentlife/app/payload/request/SignupRequest.java
+++ src/main/java/com/singleparentlife/app/payload/request/SignupRequest.java
@@ -1,9 +1,11 @@
 package com.singleparentlife.app.payload.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class SignupRequest {
 
     @JsonProperty
---
+++ src/main/java/com/singleparentlife/app/payload/response/AttachmentResponse.java
@@ -0,0 +1,11 @@
+package com.singleparentlife.app.payload.response;
+
+import lombok.Data;
+
+@Data
+public class AttachmentResponse {
+
+    private Long attachmentId;
+
+    private String attachmentType;
+}
