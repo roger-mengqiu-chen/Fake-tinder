d1e023d7f76dcc2ab5d589ae55e089db596ac6ee
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-11 12:22:10 -0700

    Update with attachment CRUD

--- src/main/java/com/singleparentlife/app/service/FileService.java
+++ src/main/java/com/singleparentlife/app/service/FileService.java
@@ -6,8 +6,11 @@ import com.singleparentlife.app.constants.Status;
 import com.singleparentlife.app.mapper.AttachmentMapper;
 import com.singleparentlife.app.mapper.ProfileMapper;
 import com.singleparentlife.app.model.Attachment;
+import com.singleparentlife.app.model.Profile;
+import com.singleparentlife.app.payload.response.AttachmentResponse;
 import com.singleparentlife.app.payload.response.JsonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,14 +30,32 @@ public class FileService {
     @Autowired
     private FileUtil fileUtil;
 
-
     public JsonResponse uploadWithProfile(Long userId, MultipartFile file){
+        if (!fileUtil.isValidImage(file)) {
+            return new JsonResponse(Status.FAIL, DataType.INVALID_IMAGE, null);
+        }
+        Profile profile = profileMapper.findByUserId(userId);
+        if (profile == null) {
+            return new JsonResponse(Status.FAIL, DataType.PROFILE_NOT_FOUND, null);
+        }
+        short profileImgAmt = profile.getProfileImgAmt();
+        // each profile should have <= 9 profile images
+        if (profileImgAmt >= 9) {
+            return new JsonResponse(Status.FAIL, DataType.TOO_MANY_PROFILE_IMG, null);
+        }
+
         try {
             // create attachment object
             Attachment attachment = fileUtil.fileToAttachment(file);
             attachment.setUserId(userId);
-
-            attachmentMapper.saveWithProfile(attachment);
+            Long attachmentId = attachmentMapper.saveWithProfile(attachment);
+            attachment.setAttachmentId(attachmentId);
+            // profile image amount need update
+            profile.increaseProfileImgAmt();
+            profileMapper.update(profile);
+            AttachmentResponse attachmentResponse = new AttachmentResponse();
+            BeanUtils.copyProperties(attachment, attachmentResponse);
+            return new JsonResponse(Status.SUCCESS, DataType.ATTACHMENT, attachmentResponse);
         } catch (IOException e) {
             log.error(e.getMessage());
             return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, "IO Exception");
@@ -42,7 +63,6 @@ public class FileService {
             log.error(e.getMessage());
             return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
         }
-        return new JsonResponse(Status.SUCCESS, DataType.PROFILE, userId);
     }
 
     public JsonResponse updateExistingAttachment(Long attachmentId, MultipartFile file) {
@@ -54,11 +74,33 @@ public class FileService {
             }
             attachment.setAttachmentId(existingAttachmentId);
             attachmentMapper.updateAttachment(attachment);
+            AttachmentResponse attachmentResponse = new AttachmentResponse();
+            BeanUtils.copyProperties(attachment, attachmentResponse);
             log.info("Attachment updated: {}", attachmentId);
-            return new JsonResponse(Status.SUCCESS, DataType.ATTACHMENT_IDS,  attachmentId);
+            return new JsonResponse(Status.SUCCESS, DataType.ATTACHMENT,  attachmentResponse);
         } catch (IOException e) {
             log.error(e.getMessage());
             return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, "IO Exception");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
+        }
+    }
+
+    public JsonResponse deleteAttachment(Long attachmentId) {
+        Long existingId = attachmentMapper.findIdOfAttachment(attachmentId);
+        Long profileId = attachmentMapper.getProfileIdOfAttachment(attachmentId);
+        Profile profile = profileMapper.findByUserId(profileId);
+
+        if (existingId == null) {
+            log.error("Attachment not found: {}", attachmentId);
+            return new JsonResponse(Status.FAIL, DataType.ATTACHMENT_NOT_FOUND, null);
+        }
+        try {
+            attachmentMapper.delete(attachmentId);
+            profile.reduceProfileImgAmt();
+            log.info("Attachment deleted: {}", attachmentId);
+            return new JsonResponse(Status.SUCCESS, DataType.ATTACHMENT_IDS,  attachmentId);
         } catch (Exception e) {
             log.error(e.getMessage());
             return new JsonResponse(Status.FAIL, DataType.SERVER_ERROR, null);
