afd25fc43feef68c4fea7631285140cc6e4dad54
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-11 12:01:33 -0700

    Add getAttachmentById

--- src/main/java/com/singleparentlife/app/controller/DownloadController.java
+++ src/main/java/com/singleparentlife/app/controller/DownloadController.java
@@ -4,7 +4,7 @@ import com.singleparentlife.app.constants.DataType;
 import com.singleparentlife.app.constants.Status;
 import com.singleparentlife.app.model.Attachment;
 import com.singleparentlife.app.payload.response.JsonResponse;
-import com.singleparentlife.app.service.DownloadService;
+import com.singleparentlife.app.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/download")
 public class DownloadController {
 
     @Autowired
-    private DownloadService downloadService;
+    private FileService fileService;
 
     @GetMapping("/message/{messageId}")
     public ResponseEntity getAttachmentByMessageId(@PathVariable Long messageId){
@@ -28,7 +30,7 @@ public class DownloadController {
             return ResponseEntity.badRequest().body(
                     new JsonResponse(Status.FAIL, DataType.INVALID_INPUT, "Message shouldn't be empty"));
         }
-        Attachment attachment = downloadService.getAttachmentByMessageId(messageId);
+        Attachment attachment = fileService.getAttachmentByMessageId(messageId);
         if (attachment == null) {
             return ResponseEntity.badRequest().body(
                     new JsonResponse(Status.FAIL, DataType.MESSAGE_NOT_FOUND, null));
@@ -42,20 +44,32 @@ public class DownloadController {
 
     @GetMapping("/profile/{userId}")
     public ResponseEntity getAttachmentByProfileId(@PathVariable Long userId){
-        //TODO
+
         if (userId == null) {
             return ResponseEntity.badRequest().body(
-                    new JsonResponse(Status.FAIL, DataType.INVALID_INPUT, "Message shouldn't be empty"));
+                    new JsonResponse(Status.FAIL, DataType.INVALID_INPUT, "UserId can't be empty")
+            );
         }
-        Attachment attachment = downloadService.getAttachmentByMessageId(userId);
+        List<Long> attachmentIds = fileService.getAttachmentByProfileId(userId);
+
+        return ResponseEntity.ok().body(
+                new JsonResponse(Status.SUCCESS, DataType.ATTACHMENT_IDS, attachmentIds)
+        );
+    }
+
+    @GetMapping("/{attachmentId}")
+    public ResponseEntity getAttachmentById(@PathVariable Long attachmentId) {
+        Attachment attachment = fileService.getAttachmentById(attachmentId);
         if (attachment == null) {
             return ResponseEntity.badRequest().body(
-                    new JsonResponse(Status.FAIL, DataType.MESSAGE_NOT_FOUND, null));
+                    new JsonResponse(Status.FAIL, DataType.ATTACHMENT_NOT_FOUND, null)
+            );
         }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(attachment.getAttachmentType()))
-                .header(HttpHeaders.CONTENT_DISPOSITION)
-                .body(attachment.getAttachmentContent());
+        else {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(attachment.getAttachmentType()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION)
+                    .body(attachment.getAttachmentContent());
+        }
     }
 }
