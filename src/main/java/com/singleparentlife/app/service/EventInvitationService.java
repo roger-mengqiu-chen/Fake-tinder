0169f59c20baaf72cd85cf57a83853f0d63fd60c
sait-wenchi <wen-chi.wu@edu.sait.ca>
2022-02-14 05:34:11 +0800

    EventInvitation: complete send/get/delete/react functions

--- src/main/java/com/singleparentlife/app/service/EventInvitationService.java
+++ src/main/java/com/singleparentlife/app/service/EventInvitationService.java
@@ -1,10 +1,12 @@
 package com.singleparentlife.app.service;
 
+import com.singleparentlife.app.constants.DataType;
 import com.singleparentlife.app.constants.Status;
 import com.singleparentlife.app.mapper.EventInvitationMapper;
 import com.singleparentlife.app.mapper.UserMapper;
 import com.singleparentlife.app.model.EventInvitation;
 import com.singleparentlife.app.payload.response.JsonResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,16 +15,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EventInvitationService {
     @Autowired
     private EventInvitationMapper eventInvitationMapper;
-    List<String> getEventId(long targetUserId){
-        //TODO
+    public JsonResponse  getEventInvitation(long targetUserId){
         List<EventInvitation> eventInvitationL = eventInvitationMapper.findByTargetUserId(targetUserId);
-        List<String> eventIdL= new ArrayList();
-        for (int i=0;i<eventInvitationL.size();i++)
-            eventIdL.add(String.valueOf(eventInvitationL.get(i).getEventId()));
-        return eventIdL;
+        if (eventInvitationL == null) {
+            return new JsonResponse(Status.FAIL, DataType.STATUS_MESSAGE, "Cannot find event Invitation");
+        } else
+        {
+            //return the List<EventInvitation> object
+            return new JsonResponse(Status.SUCCESS, DataType.LIST_OF_EVENT_INVITATION, eventInvitationL);
+        }
     }
     public JsonResponse createEventInvitation(long eventId, long targetUserId)
     {
@@ -33,4 +38,29 @@ public class EventInvitationService {
         eventInvitationMapper.save(eventInvitation);
         return new JsonResponse(Status.SUCCESS,null,null);
     }
+    public JsonResponse deleteEventInviataion(long eventId, long targetUserId) {
+        EventInvitation eventInvitation = eventInvitationMapper.findByEventIdAndTargetUserId(eventId, targetUserId);
+        if (eventInvitation == null) {
+            return new JsonResponse(Status.FAIL, DataType.STATUS_MESSAGE, "Cannot find event Invitation");
+        } else
+        {
+            eventInvitationMapper.deleteByEventInvitationId(eventInvitation.getEventInvitationId());
+            //return the eventInvitation object
+            return new JsonResponse(Status.SUCCESS, DataType.EVENT_INVITATION, eventInvitation);
+        }
+    }
+    public JsonResponse updateReactionId(long eventId, long targetUserId, short reactionId)
+    {
+        EventInvitation eventInvitation = eventInvitationMapper.findByEventIdAndTargetUserId(eventId, targetUserId);
+        if (eventInvitation == null) {
+            return new JsonResponse(Status.FAIL, DataType.STATUS_MESSAGE, "Cannot find event Invitation");
+        } else
+        {
+            eventInvitationMapper.updateReactionId(eventInvitation.getEventInvitationId(), reactionId);
+            //return the eventInvitation object
+            eventInvitation.setReactionId(reactionId);
+            return new JsonResponse(Status.SUCCESS, DataType.EVENT_INVITATION, eventInvitation);
+        }
+    }
 }
+
