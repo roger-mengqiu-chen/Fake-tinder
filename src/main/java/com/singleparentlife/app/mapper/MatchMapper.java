c0de32c02d0789bc396b32918479c3f9ce85d9c6
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-22 01:12:11 -0700

    Create MatchService

--- src/main/java/com/singleparentlife/app/mapper/MatchMapper.java
+++ src/main/java/com/singleparentlife/app/mapper/MatchMapper.java
@@ -1,13 +1,25 @@
 package com.singleparentlife.app.mapper;
 
 import com.singleparentlife.app.model.Match;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface MatchMapper {
 
-    @Insert("INSERT INTO matches (userId, targetId, reactionId) + VALUES " +
+    @Insert("INSERT INTO matches (userId, targetId, reactionId) VALUES " +
             "(#{userId}, #{targetId}, #{reactionId})")
-    long save(Match match);
+    int save(Match match);
+
+    @Select("SELECT * FROM matches " +
+            "WHERE userId = #{userId} AND targetId = #{targetId}")
+    @Results({
+            @Result(property = "userId", column = "userId"),
+            @Result(property = "targetId", column = "targetId"),
+            @Result(property = "reactionId", column = "reactionId")
+    })
+    Match findMatchBetweenUsers (Long userId, Long targetId);
+
+    @Update("UPDATE matches (reactionId) VALUES (#{reactionId}) " +
+            "WHERE userId = #{userId} AND targetId = #{targetId}")
+    int update(Match match);
 }
